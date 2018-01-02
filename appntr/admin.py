34e202a8b10b94920a9d8222e6f0e094c21ecf04
Susanna Kunzmann <s.kunzmann@ostec.de>
2018-01-02 18:12:27 +0100

    [#19] first steps for feature implementation: db migrations, creation of section within detail view

--- appntr/admin.py
+++ appntr/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Application, UserVote, Invite, Timeslot, Appointment, UserConfig
+from .models import Application, UserVote, Invite, Timeslot, Appointment, UserConfig, Feedback
 
 admin.site.register(Application)
 admin.site.register(UserVote)
 admin.site.register(Invite)
 admin.site.register(Timeslot)
 admin.site.register(Appointment)
+admin.site.register(Feedback)
 
 
 
--- appntr/models.py
+++ appntr/models.py
@@ -92,6 +92,39 @@ class Application(models.Model):
         return "nay" 
 
 
+class Feedback(models.Model):
+    class Meta:
+        app_label = 'appntr'
+
+    class TYPES:
+        YES = "yes"
+        NO = "no"
+        MAYBE = "maybe"
+        MISSED = "missed"
+        RECALL_YES = "recall_yes"
+        RECALL_NO = "recall_no"
+        RECALL_MAYBE = "recall_maybe"
+
+    added_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="feedbacks")
+    application = models.ForeignKey(Application, related_name="feedbacks")
+
+    interviewer_names = models.TextField(null=False, blank=False, verbose_name="Namen der beiden DiB-Gesprächspartner*innen")
+
+    feedback_type = models.CharField(db_index=True, max_length=25, choices= [
+        (TYPES.YES, "Ja, aufnehmen"),
+        (TYPES.NO, "Nein, nicht aufnehmen"),
+        (TYPES.MAYBE, "Wir können uns nicht einigen. Bitte erneut einladen."),
+        (TYPES.MISSED, "Person ist nicht zum Termin erschienen"),
+        (TYPES.RECALL_YES, "Gespräch einzeln geführt, meine Empfehlung: ja, aufnehmen --> bitte zweiten Gesprächspartner organisieren"),
+        (TYPES.RECALL_NO, "Gespräch einzeln geführt, meine Empfehlung: nein, nicht aufnehmen --> bitte zweiten Gesprächspartner organisieren"),
+        (TYPES.RECALL_MAYBE, "Gespräch einzeln geführt, meine Empfehlung:bin mir unsicher --> bitte zwei neue Gesprächspartner organisieren")
+    ])
+
+    statement_yes = models.TextField(null=True, blank=True, verbose_name="BEI ZUSAGE: Kompetenzen für Mitarbeit", help_text="Bitte diesen Teil nur bei Zusage ausfüllen")
+    statement_maybe = models.TextField(null=True, blank=True, verbose_name="BEI UNSICHERHEIT/ UNEINIGKEIT: Kurze Begründung", help_text="Bitte gebt den nächsten Gesprächspartnern mit auf den Weg, wo ihr unsicher seid")
+    statement_no = models.TextField(null=True, blank=True, verbose_name="BEI ABSAGE: kurze, sachliche Begründung", help_text="Bitte diesen Teil nur bei Absage ausfüllen, bitte sachlich schreiben (bspw. passt nicht zu den Werten etc.)")
+
 class UserVote(models.Model):
     class Meta:
         app_label = 'appntr'
