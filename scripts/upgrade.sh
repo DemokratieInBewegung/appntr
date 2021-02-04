39e32407d0386748d794eee3a2d34326dd47a61b
Guido Drehsen <33029486+GuidoDr@users.noreply.github.com>
2021-02-04 18:50:56 +0100

    Update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,7 +8,7 @@ SECRET_KEY=temp_value python manage.py collectstatic -v 0 --clear --noinput
 
 # Create local database migrations
 echo "Create database migrations"
-python manage.py makemigrations
+python manage.py makemigrations --merge
 
 # Apply database migrations
 echo "Apply database migrations"
