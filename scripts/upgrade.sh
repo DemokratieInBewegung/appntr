5793c7119cb55bed1c58466f8a7299f461a7185d
Guido Drehsen <33029486+GuidoDr@users.noreply.github.com>
2021-02-04 18:59:58 +0100

    rollback

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,7 +8,7 @@ SECRET_KEY=temp_value python manage.py collectstatic -v 0 --clear --noinput
 
 # Create local database migrations
 echo "Create database migrations"
-python manage.py makemigrations --merge
+python manage.py makemigrations
 
 # Apply database migrations
 echo "Apply database migrations"
