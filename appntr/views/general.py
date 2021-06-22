04ef5e31b91646c0f4de9aff295d485b19ee9cb5
michaelvoss82 <37080006+michaelvoss82@users.noreply.github.com>
2021-06-22 13:38:30 +0200

    fix for Django update

--- appntr/views/general.py
+++ appntr/views/general.py
@@ -271,8 +271,8 @@ class ListTextWidget(forms.TextInput):
         self._list = data_list
         self.attrs.update({'list':'list__%s' % self._name})
 
-    def render(self, name, value, attrs=None):
-        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
+    def render(self, name, value, attrs=None, renderer=None):
+        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs, renderer=renderer)
         data_list = '<datalist id="list__%s">' % self._name
         for item in self._list:
             data_list += '<option value="%s">' % item
