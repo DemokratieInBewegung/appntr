be69c98b5e0f9057c748842ef0d9f3747d6571e1
Guido Drehsen <guido@drehsen.com>
2022-11-10 16:21:40 +0100

    replace docker-compose with docker compose

--- scripts/send_testmail.sh
+++ scripts/send_testmail.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# run this from within running docker container (e.g. after having done "docker-compose exec web /bin/bash")
+# run this from within running docker container (e.g. after having done "docker compose exec web /bin/bash")
 
 EMAIL=$1
 if [ -z "$EMAIL" ]; then
     echo "USAGE: $0 <destination mailadress>"
     echo "       invoke this command from within a running environment, for example from"
     echo "       within a docker container (e.g. after having entered the container via"
-    echo "       'docker-compose exec web /bin/bash')."
+    echo "       'docker compose exec web /bin/bash')."
     exit 1
 fi
 
